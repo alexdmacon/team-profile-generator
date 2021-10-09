16a2f099f4a0e6ac9bd560347815c154aa518762
Alex Macon <alexdmacon@gmail.com>
2021-10-09 17:13:40 -0500

    wrote test for Employee class, no idea if it works. also exporting modules

---
+++ README.md
@@ -0,0 +1,13 @@
+https://www.npmjs.com/package/inquirer
+
+https://javascript.info/class-inheritance
+
+https://jestjs.io/docs/api
+
+https://docs.npmjs.com/specifying-dependencies-and-devdependencies-in-a-package-json-file
+
+https://www.npmjs.com/package/path
+
+https://techblog.topdesk.com/coding/frontend-testing-with-jest-assertions-deep-dive/
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/instanceof
---
+++ __tests__/Employee.test.js
@@ -0,0 +1,41 @@
+const Employee = require("../lib/Employee");
+
+describe("Employee", () => {
+  it("should create an object as an instance of the Employee class using inputted arguments as properties", () => {
+    const employee = new Employee("JoeBob", 42, "JoeBob@fake.com");
+
+    expect(employee.name).toEqual("JoeBob");
+    expect(employee.id).toEqual(42);
+    expect(employee.email).toEqual("JoeBob@fake.com");
+  });
+
+  describe("getName", () => {
+    it("should return employee name from user argument input", () => {
+      const employee = new Employee("JoeBob", 42, "JoeBob@fake.com");
+
+      expect(employee.getname()).toEqual("JoeBob");
+    });
+  });
+
+  describe("getId", () => {
+    it("should return employee ID from user argument input", () => {
+      const employee = new Employee("JoeBob", 42, "JoeBob@fake.com");
+
+      expect(employee.getId()).toEqual(42);
+    });
+  });
+
+  describe("getEmail", () => {
+    it("should return employee email from user argument input", () => {
+      const employee = new Employee("JoeBob", 42, "JoeBob@fake.com");
+
+      expect(employee.getEmail()).toEqual("JoeBob@fake.com");
+    });
+  });
+
+  describe("getRole", () => {
+    it("should return the role 'Employee'", () => {
+      expect(employee.getRole()).toEqual("Employee");
+    });
+  });
+});
