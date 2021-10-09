31e8d7a35bc457686a3945a4815308cb9180087a
Alex Macon <alexdmacon@gmail.com>
2021-10-09 17:36:20 -0500

    created test for Intern class script

---
+++ __tests__/Intern.test.js
@@ -0,0 +1,33 @@
+const Intern = require("../lib/Employee")
+
+describe("Intern", () => {
+    it("should create an Intern object inheriting properties and methods of Employee class", () => {
+      const intern = new Intern(
+        "JoeBob",
+        42,
+        "JoeBob@fake.com",
+        "UNT"
+      );
+  
+      expect(intern.school).toEqual("UNT");
+    });
+  
+    describe("getSchool", () => {
+      it("should return intern school from user argument input", () => {
+        const intern = new Intern(
+          "JoeBob",
+          42,
+          "JoeBob@fake.com",
+          "UNT"
+        );
+          expect (intern.getSchool()).toHaveReturnedWith("UNT");
+      });
+    });
+  
+    describe("getRole", () => {
+        it("should return the role 'Intern'", () => {
+            expect(intern.getRole()).toHaveReturnedWith("Intern");
+        })
+    })
+  });
+  
