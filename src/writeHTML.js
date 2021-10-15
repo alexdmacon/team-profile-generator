3418f577dca2f5d1a503ea1b701563bf42539865
Alex Macon <alexdmacon@gmail.com>
2021-10-15 00:20:35 -0500

    flailing away miserably

--- src/writeHTML.js
+++ src/writeHTML.js
@@ -1,7 +1,6 @@
 const writeHTML = (team) => {
+  console.log(`This data made it to the writeHTML function:`, team);
 
-    console.log(`This data made it to the writeHTML function:` , team);
-    console.log(team[0].name);
 
   return `<!DOCTYPE html>
     <html lang="en">
@@ -26,7 +25,12 @@ const writeHTML = (team) => {
         </div>
         <div class="container">
           <div class="row justify-content-around">
-          ${createManager}
+
+
+            ${buildTeam(team)};
+
+
+
         </div>
         </div>
     
@@ -35,18 +39,66 @@ const writeHTML = (team) => {
     `;
 };
 
-const createManager = function (manager) {
-  return ` <div class="card m-3" style="width: 18rem">
+const buildTeam = (team) => {
+  console.log("The data made it down here to the buildTeam function", team);
+    console.log("The name of this fucking guy is " , team[0].name);
+    
+    for (let i = 0; i < team.length; i++) {
+        const employee = team[i];
+        const role = employee.getRole();
+        if (role==="Manager") {
+            return buildManager(employee);
+        }
+        if (role==="Engineer") {
+            return buildEngineer(employee);
+        }
+    }
+  
+  
+  const buildManager = (manager) => {
+    return ` <div class="card m-3" style="width: 18rem">
 <div class="card-header text-white bg-primary">
   <h2>Manager</h2>
-  <h3>${manager.name}</h3>
+  <h3>${manager.getName()}</h3>
 </div>
 <ul class="list-group list-group-flush">
-  <li class="list-group-item">ID: ${manager.id}</li>
-  <li class="list-group-item">Email: ${manager.officeNumber}</li>
-  <li class="list-group-item">Office number: ${manager.officeNumber}</li>
+  <li class="list-group-item">ID: ${manager.getId()}</li>
+  <li class="list-group-item">Email: ${manager.getEmail()}</li>
+  <li class="list-group-item">Office number: ${manager.getofficeNumber()}</li>
 </ul>
 </div>`;
+  };
+
+  const buildEngineer = (engineer) => {
+    return ` <div class="card m-3" style="width: 18rem">
+    <div class="card-header text-white bg-primary">
+      <h2>Engineer</h2>
+      <h3>${engineer.getName()}</h3>
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">ID: ${engineer.getId()}</li>
+      <li class="list-group-item">Email: ${engineer.getEmail()}</li>
+      <li class="list-group-item">GitHub: ${engineer.getGithub()}</li>
+    </ul>
+    </div>`;
+  }
+
+  const buildIntern = (intern) => {
+    return ` <div class="card m-3" style="width: 18rem">
+    <div class="card-header text-white bg-primary">
+      <h2>Intern</h2>
+      <h3>${intern.getName()}</h3>
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">ID: ${intern.getId()}</li>
+      <li class="list-group-item">Email: ${intern.getEmail()}</li>
+      <li class="list-group-item">School: ${intern.getSchool()}</li>
+    </ul>
+    </div>`;
+  }
+
+
+
 };
 
 module.exports = writeHTML;
